bf007ce7b2d8e0d3cddccd88770514242940349e
jaeeunhyun <rosa2070@naver.com>
2021-04-01 16:17:30 +0900

    jjjjdgsgs

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -13,7 +13,7 @@ public class MemberApp {
     //    MemberService memberService = appConfig.memberService();
 
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        applicationContext.getBean("memberService", MemberService.class)
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
